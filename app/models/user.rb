889c992399647dfc65c9135afb08caeb382be47b
Michael Christenson <mchristenson5891@gmail.com>
2017-10-30 09:53:20 -0700

    user page working

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  has_attached_file :photo, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true, styles: { original: {convert_options: '-auto-orient'} }
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+  # validates_length_of :password, minimum: 6
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'users#index'
+
+  root "welcome#index"
+
+  resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/login', to: 'sessions#new'
+end
