77954b0e85e6ee713db8f5653579d907c60adf9e
Michael Christenson <mchristenson5891@gmail.com>
2017-10-30 15:14:00 -0700

    user created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,25 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+          redirect_to user_path(@user.id)
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to root_path, notice: 'Logged out!'
+    end
+
     private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :photo, :id, :description)
